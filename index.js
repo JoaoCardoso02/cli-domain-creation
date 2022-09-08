797284927dc96eb8d4d9deee696a9991570e58e7
João Cardoso <joaolucas.cardoso02@gmail.com>
2022-09-08 11:55:18 -0300

    feat: add files creation

--- index.js
+++ index.js
@@ -5,23 +5,10 @@ const pluralize = require('pluralize')
 
 function createDomain() {
 	const { domainName, pluralDomainName } = getDomainNameAndPluralDomainName()
-	createDomainFolder(pluralDomainName)
+	createDomainFolder(domainName)
+	createDomainFiles(domainName)
 }
 
-function createDomainFolder(domainName) {
-	checkAndCreateFolder(path.resolve('.'), 'domain')
-	checkAndCreateFolder(path.resolve('.', 'domain'), domainName)
-	checkAndCreateFolder(path.resolve('.', 'domain', domainName), 'entities')
-	checkAndCreateFolder(path.resolve('.', 'domain', domainName), 'services')
-	checkAndCreateFolder(path.resolve('.', 'domain', domainName), 'infrastructure')
-	checkAndCreateFolder(path.resolve('.', 'domain', domainName), 'types')
-}
-
-function checkAndCreateFolder(pathName, folderName) {
-	if (!fs.existsSync(path.resolve(pathName, folderName))) {
-		fs.mkdirSync(path.resolve(pathName, folderName))
-	}
-}
 
 function getDomainNameAndPluralDomainName() {
 	const domainName = getDomainName()
@@ -47,6 +34,48 @@ function getPluralDomainName(name) {
 	if (pluralDomainName) return pluralDomainName
 
 	return pluralize(name)
+}
+
+function createDomainFolder(domainName) {
+	checkAndCreateFolder(path.resolve('.'), 'domain')
+	checkAndCreateFolder(path.resolve('.', 'domain'), domainName)
+	checkAndCreateFolder(path.resolve('.', 'domain', domainName), 'entities')
+	checkAndCreateFolder(path.resolve('.', 'domain', domainName), 'services')
+	checkAndCreateFolder(path.resolve('.', 'domain', domainName), 'infrastructure')
+	checkAndCreateFolder(path.resolve('.', 'domain', domainName), 'types')
+
+	checkAndCreateFolder(path.resolve('.', 'domain', domainName, 'entities'), '__tests__')
+	checkAndCreateFolder(path.resolve('.', 'domain', domainName, 'services'), '__tests__')
+	checkAndCreateFolder(path.resolve('.', 'domain', domainName, 'infrastructure'), '__tests__')
+}
+
+function checkAndCreateFolder(pathName, folderName) {
+	if (!fs.existsSync(path.resolve(pathName, folderName))) {
+		fs.mkdirSync(path.resolve(pathName, folderName))
+	}
+}
+
+function createDomainFiles(domainName) {
+	const domainNameCapitalized = capitalizeString(domainName)
+
+	checkAndCreateFile(path.resolve('.', 'domain', domainName, 'entities'), `${domainNameCapitalized}.ts`)
+	checkAndCreateFile(path.resolve('.', 'domain', domainName, 'services'), `${domainNameCapitalized}Service.ts`)
+	checkAndCreateFile(path.resolve('.', 'domain', domainName, 'infrastructure'), `${domainNameCapitalized}Repository.ts`)
+	checkAndCreateFile(path.resolve('.', 'domain', domainName, 'types'), `I${domainNameCapitalized}.ts`)
+
+	checkAndCreateFile(path.resolve('.', 'domain', domainName, 'entities', '__tests__'), `${domainNameCapitalized}.spec.ts`)
+	checkAndCreateFile(path.resolve('.', 'domain', domainName, 'services', '__tests__'), `${domainNameCapitalized}Service.spec.ts`)
+	checkAndCreateFile(path.resolve('.', 'domain', domainName, 'infrastructure', '__tests__'), `${domainNameCapitalized}Repository.spec.ts`)
+}
+
+function checkAndCreateFile(pathName, fileName) {
+	if (!fs.existsSync(path.resolve(pathName, fileName))) {
+		fs.writeFileSync(path.resolve(pathName, fileName), 'something')
+	}
+}
+
+function capitalizeString(name) {
+	return name.charAt(0).toUpperCase() + name.slice(1);
 }
 
 createDomain()
